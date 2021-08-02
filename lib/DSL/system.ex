8db6155c3958f43a6ed0c585df35321d9bc8e879
Digit <woof@puppy.surf>
2021-08-02 08:40:19 -0700

    systems DSL to get/set component data

--- lib/DSL/system.ex
+++ lib/DSL/system.ex
@@ -8,13 +8,23 @@ defmodule ElixirRPG.DSL.System do
   defmacro defsystem(name, do: block) do
     quote do
       defmodule ElixirRPG.RuntimeSystems.unquote(name) do
+        alias ElixirRPG.Entity
+
         Module.register_attribute(__MODULE__, :wants, accumulate: true, persist: true)
         Module.register_attribute(__MODULE__, :system_name, persist: true)
 
-        defp get_component_data(component_type, key) do
+        defp __get_component_data(entity_pid, component_type, key) do
+          case GenServer.call(entity_pid, {:get_component, component_type}) do
+            {:ok, component} ->
+              get_in(component, key)
+
+            _ ->
+              nil
+          end
         end
 
-        defp set_component_data(component_tupe, key, new_value) do
+        defp __set_component_data(entity_pid, component_type, key, new_value) do
+          GenServer.call(entity_pid, {:set_component_member, component_type, key, new_value})
         end
 
         unquote(block)
@@ -27,6 +37,18 @@ defmodule ElixirRPG.DSL.System do
           @system_name
         end
       end
+    end
+  end
+
+  defmacro get_component_data(component_type, key) do
+    quote do
+      __get_component_data(var!(entity), unquote(component_type), unquote(key))
+    end
+  end
+
+  defmacro set_component_data(component_type, key, new_data) do
+    quote do
+      __set_component_data(var!(entity), unquote(component_type), unquote(key), unquote(new_data))
     end
   end
 
@@ -44,9 +66,11 @@ defmodule ElixirRPG.DSL.System do
 
   defmacro on_tick(do: block) do
     quote do
-      def __tick() do
-        unquote(block)
-        :tock
+      def __tick(var!(entity_list)) when is_list(var!(entity_list)) do
+        Enum.each(var!(entity_list), fn var!(entity) ->
+          _ = var!(entity)
+          unquote(block)
+        end)
       end
     end
   end
